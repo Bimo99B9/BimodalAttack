1842b03ba9f00369f950aace3899a5deff5f26dd
Daniel <daniel.lopezgala@epfl.ch>
2025-03-19 18:44:35 +0100

    :sparkles: New exps

--- run_experiments.sh
+++ run_experiments.sh
@@ -4,82 +4,82 @@ set -e
 
 # Dynamic Search Configuration
 CUDA_VISIBLE_DEVICES=1 python experiments.py \
-    --name "Dynamic Search" \
+    --name "Dynamic Search 2" \
     --num_steps 250 \
     --search_width 512 \
     --dynamic_search True \
     --min_search_width 32 \
     --pgd_attack True \
     --gcg_attack True \
-    --alpha "2/255" \
-    --eps "66/255" \
+    --alpha "4/255" \
+    --eps "32/255" \
     --debug_output False \
-    >experiments_dynamic_wout_image.out 2>&1
+    >experiments_dynamic_wout_image2.out 2>&1
 
 # Base Configuration
 CUDA_VISIBLE_DEVICES=1 python experiments.py \
-    --name "Base Configuration" \
+    --name "Base Configuration 2" \
     --num_steps 250 \
     --search_width 512 \
     --dynamic_search False \
     --min_search_width 512 \
     --pgd_attack True \
     --gcg_attack True \
-    --alpha "2/255" \
-    --eps "66/255" \
-    --debug_output False \
-    >experiments_base_wout_image2.out 2>&1
-
-# GCG Only Configuration
-CUDA_VISIBLE_DEVICES=1 python experiments.py \
-    --name "GCG Only" \
-    --num_steps 250 \
-    --search_width 512 \
-    --dynamic_search False \
-    --min_search_width 512 \
-    --pgd_attack False \
-    --gcg_attack True \
-    --alpha "2/255" \
-    --eps "64/255" \
-    --debug_output False \
-    >experiments_gcg.out 2>&1
-
-# PGD Only Configuration
-CUDA_VISIBLE_DEVICES=1 python experiments.py \
-    --name "PGD Only 1" \
-    --num_steps 600 \
-    --search_width 0 \
-    --dynamic_search False \
-    --min_search_width 0 \
-    --pgd_attack True \
-    --gcg_attack False \
-    --alpha "2/255" \
-    --eps "16/255" \
-    --debug_output True \
-    >experiments_pgd1.out 2>&1
-
-CUDA_VISIBLE_DEVICES=1 python experiments.py \
-    --name "PGD Only 2" \
-    --num_steps 600 \
-    --search_width 0 \
-    --dynamic_search False \
-    --min_search_width 0 \
-    --pgd_attack True \
-    --gcg_attack False \
     --alpha "4/255" \
     --eps "32/255" \
-    --debug_output True \
-    >experiments_pgd2.out 2>&1
+    --debug_output False \
+    >experiments_base_wout_image3.out 2>&1
 
-CUDA_VISIBLE_DEVICES=1 python experiments.py \
-    --name "PGD Quick" \
-    --num_steps 100 \
-    --search_width 0 \
-    --dynamic_search False \
-    --min_search_width 0 \
-    --pgd_attack True \
-    --gcg_attack False \
-    --alpha "16/255" \
-    --eps "128/255" \
-    --debug_output True \
-    >experiments_pgd3.out 2>&1
+# # GCG Only Configuration
+# CUDA_VISIBLE_DEVICES=1 python experiments.py \
+#     --name "GCG Only" \
+#     --num_steps 250 \
+#     --search_width 512 \
+#     --dynamic_search False \
+#     --min_search_width 512 \
+#     --pgd_attack False \
+#     --gcg_attack True \
+#     --alpha "2/255" \
+#     --eps "64/255" \
+#     --debug_output False \
+#     >experiments_gcg.out 2>&1
+
+# # PGD Only Configuration
+# CUDA_VISIBLE_DEVICES=1 python experiments.py \
+#     --name "PGD Only 1" \
+#     --num_steps 600 \
+#     --search_width 0 \
+#     --dynamic_search False \
+#     --min_search_width 0 \
+#     --pgd_attack True \
+#     --gcg_attack False \
+#     --alpha "2/255" \
+#     --eps "16/255" \
+#     --debug_output True \
+#     >experiments_pgd1.out 2>&1
+
+# CUDA_VISIBLE_DEVICES=1 python experiments.py \
+#     --name "PGD Only 2" \
+#     --num_steps 600 \
+#     --search_width 0 \
+#     --dynamic_search False \
+#     --min_search_width 0 \
+#     --pgd_attack True \
+#     --gcg_attack False \
+#     --alpha "4/255" \
+#     --eps "32/255" \
+#     --debug_output True \
+#     >experiments_pgd2.out 2>&1
+
+# CUDA_VISIBLE_DEVICES=1 python experiments.py \
+#     --name "PGD Quick" \
+#     --num_steps 100 \
+#     --search_width 0 \
+#     --dynamic_search False \
+#     --min_search_width 0 \
+#     --pgd_attack True \
+#     --gcg_attack False \
+#     --alpha "16/255" \
+#     --eps "128/255" \
+#     --debug_output True \
+#     >experiments_pgd3.out 2>&1
