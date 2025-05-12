3ae989c70395688ab899ef6fbfd58b532fc70fda
Daniel <daniel.lopezgala@epfl.ch>
2025-05-12 18:27:49 +0200

    Debug gcg

--- run_experiments.sh
+++ run_experiments.sh
@@ -2,20 +2,20 @@
 # Ensure the script exits if any command fails
 set -e
 
-CUDA_VISIBLE_DEVICES=5 python experiments.py \
-    --name "Llava-RC - PGD Only" \
+CUDA_VISIBLE_DEVICES=3 python experiments.py \
+    --name "Llava-RC - Joint" \
     --num_steps 600 \
     --search_width 0 \
     --dynamic_search False \
     --min_search_width 0 \
     --pgd_attack True \
-    --gcg_attack False \
+    --gcg_attack True \
     --alpha "4/255" \
     --eps "64/255" \
     --debug_output False \
-    --joint_eval False \
-    --model "llava-rc" \
-    > logs/experiments_llavarc_pgd.out 2>&1
+    --joint_eval True \
+    --model "gemma" \
+    > logs/experiments_llavarc_joint.out 2>&1
 
 # TODO:
 # CUDA_VISIBLE_DEVICES=6 python experiments.py \
